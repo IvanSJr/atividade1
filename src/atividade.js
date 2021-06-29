d6be12feebb16a948cbcd599fab9a90ca500b200
IvanSJr <ivanjrjesus01@gmail.com>
2021-06-29 11:52:34 -0300

    Incrementando if para que informe quando delta for negativo que não existem raizes reais.

--- src/atividade.js
+++ src/atividade.js
@@ -4,8 +4,12 @@ let c = prompt("Digite o valor de C: ")
 const A = parseFloat(a)
 const B = parseFloat(b)
 const C = parseFloat(c)
-
+console.log("A =", A,"B =" ,B,"C =" ,C)
 let delta = B*B - 4 * A * C
-let x1 = -B + Math.sqrt(delta) / (2 * A)
-let x2 = B - Math.sqrt(delta) / (2 * A)
-console.log(`X': ${x1}\nX'': ${x2}`)
+if(delta < 0){
+    console.log(`Delta é negativo ${delta}. Por isso não existem raizes reais`)
+}else{
+    let x1 = (-B + Math.sqrt(delta)) / (2 * A)
+    let x2 = (-B - Math.sqrt(delta)) / (2 * A)
+    console.log(`X': ${x1}\nX'': ${x2}`)
+}
