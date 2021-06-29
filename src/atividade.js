a1ece3e8a0997d6705ffd62bdcc347f00e55e66c
IvanSJr <ivan.junior2706@gmail.com>
2021-06-28 22:28:31 -0300

    Subindo com erro de NaN.

---
+++ src/atividade.js
@@ -0,0 +1,12 @@
+let a = prompt("Digite o valor de A: ")
+let b = prompt("Digite o valor de B: ")
+let c = prompt("Digite o valor de C: ")
+const A = parseFloat(a)
+const B = parseFloat(b)
+const C = parseFloat(c)
+
+let delta = B*B - 4 * A * C
+let x1 = -B + Math.sqrt(delta) / (2 * A)
+let x2 = B - Math.sqrt(delta) / (2 * A)
+console.log(`X': ${x1}\nX'': ${x2}`)
+console.log(i)
